2667a2c35d39f02fa8ae5cf664393dab484e428b
Diego Ferreira <diego@coyote.inf.br>
2025-05-11 20:17:14 -0300

    config: add nativewind config

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -2,8 +2,8 @@ import { View, Text } from 'react-native';
 
 export default function Home() {
     return (
-        <View>
-            <Text>Home</Text>
+        <View className='flex-1 items-center justify-center bg-red-600'>
+            <Text>Teste</Text>
         </View>
     );
 }
